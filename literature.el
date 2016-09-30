ce47cd5efb100e17e07e8379ed5f47e2d5b02896
William F. Broderick <billbrod@gmail.com>
2016-09-30 17:57:56 -0400

    Removes python
    This commit removes the python and information, as well as correcting
    some weird indentation in literature.el

--- literature.el
+++ literature.el
@@ -715,8 +715,8 @@ bib are staged and committed."
 ;;; path as appropriate.
 
 ;;;###autoload
-    (defun literature-test-bib-files ()
-      "This is a function to test your bib files in case something's
+(defun literature-test-bib-files ()
+  "This is a function to test your bib files in case something's
    the matter with your library and helm-bibtex. I ran into an
    error where helm-bibtex would not display my library but also
    returned no errors. Evaluating bibtex-completion-candidates
@@ -726,78 +726,78 @@ bib are staged and committed."
    a problem, so you can go and look at it specifically. For me,
    it was a non-standard parentheses (appeared too large) which
    was causing the issue."
-      (let test-list (f-glob "~/Org-Docs/Papers/*/*.bib")
-	   (while test-list
-	     (setq bibtex-completion-bibliography (car test-list))
-	     (print (car test-list))
-	     (print (bibtex-completion-candidates))
-	     (setq test-list (cdr test-list)))))
+  (let test-list (f-glob "~/Org-Docs/Papers/*/*.bib")
+       (while test-list
+	 (setq bibtex-completion-bibliography (car test-list))
+	 (print (car test-list))
+	 (print (bibtex-completion-candidates))
+	 (setq test-list (cdr test-list)))))
 
 
 ;;; CUSTOMIZATION OF EXISTING PACKAGES
-    ;; This section contains some code to make org-ref and helm-bibtex
-    ;; work well with the structure of my bibliography.
+;; This section contains some code to make org-ref and helm-bibtex
+;; work well with the structure of my bibliography.
 
-    ;; We need this because org-ref-get-pdf-filename-function expects one
-    ;; pdf, but bibtex-completion-find-pdf returns a list containing one
-    ;; filename. So, using car, we grab the first item in that list and
-    ;; return it.
-    (defun my/find-one-pdf (key)
-      (car (bibtex-completion-find-pdf key))
-      )
+;; We need this because org-ref-get-pdf-filename-function expects one
+;; pdf, but bibtex-completion-find-pdf returns a list containing one
+;; filename. So, using car, we grab the first item in that list and
+;; return it.
+(defun my/find-one-pdf (key)
+  (car (bibtex-completion-find-pdf key))
+  )
 
-    (setq org-ref-get-pdf-filename-function 'my/find-one-pdf)
+(setq org-ref-get-pdf-filename-function 'my/find-one-pdf)
 
-    ;; Over-write the bibtex-completion-edit-notes function, because we
-    ;; format the entries as KEY/KEY.org in bibtex-completion-notes-path,
-    ;; which there's currently no support for.
-    (eval-after-load 'helm-bibtex		
-      '(defun bibtex-completion-edit-notes (key)
-	 "Open the notes associated with the entry key (using `find-file'). Will look for \"KEY/KEY/org\" in `bibtex-completion-notes-path'."
-	 (if (f-directory? bibtex-completion-notes-path)
-	     ;; One notes file per publication: just open the file.
-	     (let ((path (f-join bibtex-completion-notes-path
-				 (s-concat key "/" key bibtex-completion-notes-extension))))
-	       (find-file path)
-	       (unless (f-exists? path)
-		 (insert (s-format bibtex-completion-notes-template-multiple-files
-				   'bibtex-completion-apa-get-value
-				   (bibtex-completion-get-entry key)))))
-	   (unless (and buffer-file-name
-			(f-same? bibtex-completion-notes-path buffer-file-name))
-	     (find-file-other-window bibtex-completion-notes-path))
-	   (widen)
-	   (show-all)
-	   (goto-char (point-min))
-	   (if (re-search-forward (format bibtex-completion-notes-key-pattern key) nil t)
+;; Over-write the bibtex-completion-edit-notes function, because we
+;; format the entries as KEY/KEY.org in bibtex-completion-notes-path,
+;; which there's currently no support for.
+(eval-after-load 'helm-bibtex		
+  '(defun bibtex-completion-edit-notes (key)
+     "Open the notes associated with the entry key (using `find-file'). Will look for \"KEY/KEY/org\" in `bibtex-completion-notes-path'."
+     (if (f-directory? bibtex-completion-notes-path)
+	 ;; One notes file per publication: just open the file.
+	 (let ((path (f-join bibtex-completion-notes-path
+			     (s-concat key "/" key bibtex-completion-notes-extension))))
+	   (find-file path)
+	   (unless (f-exists? path)
+	     (insert (s-format bibtex-completion-notes-template-multiple-files
+			       'bibtex-completion-apa-get-value
+			       (bibtex-completion-get-entry key)))))
+       (unless (and buffer-file-name
+		    (f-same? bibtex-completion-notes-path buffer-file-name))
+	 (find-file-other-window bibtex-completion-notes-path))
+       (widen)
+       (show-all)
+       (goto-char (point-min))
+       (if (re-search-forward (format bibtex-completion-notes-key-pattern key) nil t)
                                         ; Existing entry found:
-	       (when (eq major-mode 'org-mode)
-		 (org-narrow-to-subtree)
-		 (re-search-backward "^\*+ " nil t)
-		 (org-cycle-hide-drawers nil)
-		 (bibtex-completion-notes-mode 1))
+	   (when (eq major-mode 'org-mode)
+	     (org-narrow-to-subtree)
+	     (re-search-backward "^\*+ " nil t)
+	     (org-cycle-hide-drawers nil)
+	     (bibtex-completion-notes-mode 1))
                                         ; Create a new entry:
-	     (let ((entry (bibtex-completion-get-entry key)))
-	       (goto-char (point-max))
-	       (insert (s-format bibtex-completion-notes-template-one-file
-				 'bibtex-completion-apa-get-value
-				 entry)))
-	     (when (eq major-mode 'org-mode)
-	       (org-narrow-to-subtree)
-	       (re-search-backward "^\*+ " nil t)
-	       (org-cycle-hide-drawers nil)
-	       (goto-char (point-max))
-	       (bibtex-completion-notes-mode 1))))))
+	 (let ((entry (bibtex-completion-get-entry key)))
+	   (goto-char (point-max))
+	   (insert (s-format bibtex-completion-notes-template-one-file
+			     'bibtex-completion-apa-get-value
+			     entry)))
+	 (when (eq major-mode 'org-mode)
+	   (org-narrow-to-subtree)
+	   (re-search-backward "^\*+ " nil t)
+	   (org-cycle-hide-drawers nil)
+	   (goto-char (point-max))
+	   (bibtex-completion-notes-mode 1))))))
 
 
 
-    ;;Custom function to open the individual notes file
-    (add-to-list 'org-ref-helm-user-candidates
-		 '("Open individual notes file" . (lambda ()
-						    (bibtex-completion-edit-notes (car (org-ref-get-bibtex-key-and-file))))))
+;;Custom function to open the individual notes file
+(add-to-list 'org-ref-helm-user-candidates
+	     '("Open individual notes file" . (lambda ()
+						(bibtex-completion-edit-notes (car (org-ref-get-bibtex-key-and-file))))))
 
 
-    (provide 'literature)
+(provide 'literature)
 
 ;;; literature.el ends here
 
